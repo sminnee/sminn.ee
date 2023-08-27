caa62107d6887466ae87600e4c3cc3e24a82cb7d
Sam Minnée <sam.minnee@gmail.com>
2023-08-27 21:28:56 +1200

    feat: Atom feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 // Import prior to `module.exports` within `.eleventy.js`
 const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  eleventyConfig.addPlugin(pluginRss, {
+    posthtmlRenderOptions: {
+      closingSingleTag: "default", // opt-out of <img/>-style XHTML single tags
+    },
   });
 
   eleventyConfig.addPassthroughCopy("css/*");
